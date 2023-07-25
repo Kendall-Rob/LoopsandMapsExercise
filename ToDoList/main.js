bf751f55b35703389e4b990c51ca1703f35deb9e
Kendall Robinson <kentechs91@gmail.com>
2023-07-25 15:27:49 -0400

    first commit

--- ToDoList/main.js
+++ ToDoList/main.js
@@ -1,32 +1,3 @@
-/*let selectedItem = document.querySelector('li.selected');
-let first = selectedItem.previousElementSibling;
-let last = selectedItem.nextElementSibling;
-let list = selectedItem.parentElement;
-let header = selectedItem.parentElement.parentElement;
-let section = selectedItem.parentElement.parentElement.nextElementSibling;
-
-console.log(selectedItem.parentElement);
-console.log(first);
-console.log(last);
-console.log(list);
-console.log(header);*/
-
-/*let h = document.querySelector('header');
-        console.log(h);
-let s = h.nextElementSibling.children; 
-        console.log(s); 
-let sc = document.querySelector('section.current');
-        console.log(sc);
-let sc1 = sc.nextElementSibling;
-        console.log(sc1);
-let sc2 = sc.previousElementSibling.children[0];
-        console.log(sc2);
-let sc3 = sc.parentElement;
-        console.log(sc3);
-let sc4 = document.querySelectorAll('section h2');
-        console.log(sc4);*/
-
-
 let inputBox = document.getElementById("input-box");
 let listContainer = document.getElementById("list-container");
 
@@ -52,3 +23,5 @@ listContainer.addEventListener("click", function(e) {
         e.target.parentElement.remove();
        }
 }, false);
+
+
